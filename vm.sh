9121c3575e049b08b6cc211148f137a161ef3444
Giyu <72243953+afnan007a@users.noreply.github.com>
2021-09-03 15:55:34 +0530

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -1,15 +1,62 @@
 #!/bin/bash
+echo
+echo getting the script ready...
+sleep 1
+echo ...Done
+echo
+echo making some coffee for giyu...
+sleep 0.7
+echo ..Done
+echo
+echo making more coffee...
+sleep 0.5
+echo ..Done
+echo
+echo Logging In to the vm...
+sleep 0.2
+echo ..Done
+
+
+sleep 1
+echo
+echo ==== { Ptero-vm } ====
+echo
+sleep 1
+
+echo
+echo DISK SPACE CHECKUP:
+echo
+space_free=$( df -h | awk '{ print $5 }' | sort -n | tail -n 1 | sed 's/%//' )
+case $space_free in
+[1-5]*)
+echo Plenty of disk space available
+;;
+[6-7]*)
+echo There could be a problem in the near future
+;;
+8*)
+echo Maybe we should look at clearing out old files
+;;
+9*)
+echo We could have a serious problem on our hands soon
+;;
+*)
+echo Something is not quite right here
+;;
+esac
+
+sleep 1
+echo
+echo
 echo "the vm has been started c;"
 docmd() {
-    read -p "user@MyVPS:~ " CMD
+    read -p "default@Ptero-vm:~ " CMD
     eval "$CMD"
-    echo "user@MyVPS:~ "
     docmd2
 }
 docmd2() {
-    read -p "user@MyVPS:~ " CMD2
+    read -p "default@Ptero-vm:~ " CMD2
     eval "$CMD2"
-    echo "user@MyVPS:~ "
     docmd
 }
 docmd
